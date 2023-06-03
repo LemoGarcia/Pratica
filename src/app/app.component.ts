d0bbde1dd72ed6b3ca96ff8d1c04b60ea655d8ff
LemoGarcia <lemosgarcia22@hotmail.com>
2023-06-03 17:41:14 +0100

    meu commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pratica';
-}
+export class AppComponent implements OnInit{
+
+  lemo = 'Idiota';
+  gualter = 22;
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  }
